969e51f1115b221ca9b4aec940470071bff8978e
Amarnadha Reddy Kolla <141890598+Amarnadh31@users.noreply.github.com>
2025-02-08 20:19:47 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-# set -e
+set -e
 
-# # Ensure Docker is using the correct socket
-# export DOCKER_HOST="unix:///var/run/docker.sock"
+# Ensure Docker is using the correct socket
+export DOCKER_HOST="unix:///var/run/docker.sock"
 
-# # Get the list of running containers
-# running_containers=$(docker ps -q)
+# Get the list of running containers
+running_containers=$(docker ps -q)
 
-# if [ -n "$running_containers" ]; then
-#     echo "Stopping running containers..."
-#     docker stop $running_containers
+if [ -n "$running_containers" ]; then
+    echo "Stopping running containers..."
+    docker stop $running_containers
 
-#     echo "Removing all stopped containers..."
-#     docker rm -f $(docker ps -aq)
-# else
+    echo "Removing all stopped containers..."
+    docker rm -f $(docker ps -aq)
+else
     echo "No running containers found. Skipping stop step."
-# fi
+fi
 
 # docker stop $(docker ps -q) && docker rm -f $(docker ps -aq)
