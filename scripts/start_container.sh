48d33662b415cfaa0e549ce421b83dde5dbbb94f
Amarnadha Reddy Kolla <141890598+Amarnadh31@users.noreply.github.com>
2025-02-08 20:24:21 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -6,3 +6,12 @@ docker pull amarnadh31/mysql
 
 # Run the Docker image as a container
 docker run -d --name mysql amarnadh31/mysql 
+
+docker pull amarnadh31/backend
+
+
+docker run -d --name mysql amarnadh31/backend
+
+docker pull amarnadh31/frontend
+
+docker run -d --name mysql amarnadh31/frontend
