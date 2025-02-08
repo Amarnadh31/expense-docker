0edc756ebab2ce86c4f18617c87d7fa415b0649c
Amarnadha Reddy Kolla <141890598+Amarnadh31@users.noreply.github.com>
2025-02-08 19:47:42 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -6,9 +6,10 @@ set -e
 running_containers=$(docker ps -q)
 
 if [ -n "$running_containers" ]; then
-    echo "Stopping and removing containers..."
-    docker stop $running_containers && docker rm -f $(docker ps -aq)
+    echo "Stopping and removing running containers..."
+    docker stop $running_containers
+    docker rm -f $(docker ps -aq)
 else
-    echo "No running containers to stop."
+    echo "No running containers found. Skipping stop step."
 fi
 # docker stop $(docker ps -q) && docker rm -f $(docker ps -aq)
